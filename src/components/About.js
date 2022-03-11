f56bb8d0666ac3eb9e9193ed0e47b027209dee86
tpurdon <Tom.Purdon@Verint.com>
2022-03-11 16:01:42 +0000

    fixed image ref again for gh pages

--- src/components/About.js
+++ src/components/About.js
@@ -31,7 +31,7 @@ export default function Contact() {
             <img
                 className="object-cover object-center rounded"
                 alt="hero"
-                src="./my-react-cv/public/me.jpg"
+                src=".\my-react-cv\.\.\me.JPG"
             />
             </div>
         </div>
